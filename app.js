00d769af776af07a029668007405fa3e62e3a802
jsyoon415 <jslab415@naver.com>
2023-12-09 22:12:44 +0900

    api를 위한 urlencoded 업데이트, 어카운트 루트 추가

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('public'));
 app.use(expressLayouts);
 app.use('/', require('./routes/main'));
 app.use('/', require('./routes/problem'));
 app.use('/', require('./routes/api'));
+app.use('/', require('./routes/account'));
 
 app.listen(port, () => {
     console.log(`${port}번 포트에서 실행중`);
